a853fd4b61310f977a41e10c76085f92518ac142
EgorVoron <33517421+EgorVoron@users.noreply.github.com>
2019-06-14 18:14:49 +0300

    Create splitter.py

---
+++ splitter.py
@@ -0,0 +1,50 @@
+import argparse
+import moviepy
+from moviepy.editor import *
+from skimage.measure import compare_ssim
+import cv2
+import imageio
+import numpy as np
+
+
+def get_diff(im1, im2, mode='gray'):
+    im1 = cv2.resize(im1, (300, 300))
+    im2 = cv2.resize(im2, (300, 300))
+    if mode == 'color':
+        im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+        im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+    (score, diff) = compare_ssim(im1, im2, full=True)
+    diff = (diff * 255).astype("uint8")
+    return score
+
+
+class Splitter:
+    def __init__(self, video_path):
+        self.v = VideoFileClip(video_path)
+        self.len = self.v.duration
+        self.fps = self.v.fps
+        self.frame_time = 1 / self.fps
+        self.points = [0,]
+        
+        
+    def get_points(self):
+        i = self.frame_time
+        while (i < self.len):
+            d = get_diff(self.v.get_frame(i - (self.frame_time), 
+                         self.v.get_frame(i), mode='color'))
+            self.frames.append(self.v.get_frame(i - (self.frame_time)))
+            i += self.frame_time
+            if d < 0.55:
+                self.points.append(round(i * self.frame_time))
+                
+                
+    def make_videos(self, path_to_save):
+        
+        def write_video(finals, save_path, fps=self.fps):
+            writer = imageio.get_writer(save_path, fps=fps)
+            for final_img in finals:
+                writer.append_data(final_img)
+            writer.close()
+            
+        for i in range(len(self.points)):
+            write_video(self.points[i:i+1], path_to_save + '/' + str(i), self.fps)
