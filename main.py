ce8f4e4e1c0c3c5d25f06342475d4b45ed17d4ac
EgorVoron <33517421+EgorVoron@users.noreply.github.com>
2019-06-14 20:45:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("video_path", help="")
 parser.add_argument("path_where_to_save", help="")
 args = parser.parse_args()
+
 splitter = Splitter(args.video_path)
 splitter.get_points()
 splitter.make_videos(args.path_where_to_save)
