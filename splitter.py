900834cb3c70e1f6b5fbd426682a18dfb41bf73c
EgorVoron <33517421+EgorVoron@users.noreply.github.com>
2019-06-14 20:29:44 +0300

    Update splitter.py

--- splitter.py
+++ splitter.py
@@ -5,7 +5,7 @@ from skimage.measure import compare_ssim
 import cv2
 import imageio
 import numpy as np
-
+from time import time
 
 def get_diff(im1, im2, mode='gray'):
     im1 = cv2.resize(im1, (300, 300))
@@ -24,19 +24,35 @@ class Splitter:
         self.len = self.v.duration
         self.fps = self.v.fps
         self.frame_time = 1 / self.fps
-        self.points = [0,]
+        self.points = [0,] # кадры после монтажки
+        self.frames = []
         
         
     def get_points(self):
         i = self.frame_time
+        print('Start')
         while (i < self.len):
-            d = get_diff(self.v.get_frame(i - (self.frame_time), 
-                         self.v.get_frame(i), mode='color'))
-            self.frames.append(self.v.get_frame(i - (self.frame_time)))
+            getto = self.v.get_frame(i - self.frame_time)
+            d = get_diff(getto, 
+                         self.v.get_frame(i), mode='color')
+            self.frames.append(getto)
+            
             i += self.frame_time
-            if d < 0.55:
-                self.points.append(round(i * self.frame_time))
+            if d < 0.55: # 45 is better
+                print(d)
+                self.points.append(round(i * self.fps))
+            
                 
+    def make_videos(self, path_to_save):         
+        def write_video(finals, save_path, fps=self.fps):
+            writer = imageio.get_writer(save_path, fps=fps)
+            for final_img in finals:
+                writer.append_data(final_img)
+            writer.close()
+
+        for i in range(len(self.points) - 1):
+            print('Writing')
+            write_video(self.frames[self.points[i]:self.points[i+1]-1], path_to_save + '/' + str(i) + '.mp4', self.fps)
                 
     def make_videos(self, path_to_save):
         
