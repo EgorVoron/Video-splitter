16e30341accde3176a3c03a976581c9b170a4c6e
EgorVoron <33517421+EgorVoron@users.noreply.github.com>
2019-11-17 14:49:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("video_path", help="")
+parser.add_argument("mode", help="Save video parts? [y]/[n]")
 parser.add_argument("path_where_to_save", help="")
 args = parser.parse_args()
 
 splitter = Splitter(args.video_path)
 splitter.find_points()
-splitter.make_videos(args.path_where_to_save)
-print('Videos are saved')
+if mode == 'y':
+  splitter.make_videos(args.path_where_to_save)
+  print('Videos are saved')
+else:
+  print(splitter.get_points())
+
