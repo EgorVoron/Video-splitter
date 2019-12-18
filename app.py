50b529857eec8ffc93f39a278d08156d5b7f9346
EgorVoron <ekvoron01@gmail.com>
2019-12-18 10:32:18 +0300

    :zap: Adapt for the new splitter structure

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, send_file
 import os
-from splitter import Splitter
+from splitter import run_splitter, write_videos
 
 app = Flask(__name__)
 
@@ -12,17 +12,28 @@ def save_video(request):
     return ''
 
 
+def zip_videos(videos_path):
+    return 0
+
+
 @app.route('/time_points', methods=['POST'])
-def time_points():
+def get_time_points():
     filename = save_video(request=request)
-    splitter = Splitter(video_path=os.path.join(path_to_save, filename))
-    splitter.find_points()
-    return jsonify({'points': splitter.get_time_points()})
+    time_points, frame_points, frames = run_splitter(num_threads=3, file_path=filename)
+    return jsonify({'time_points': time_points})
 
 
 @app.route('/frame_points', methods=['POST'])
-def frame_points():
+def get_frame_points():
     filename = save_video(request=request)
-    splitter = Splitter(video_path=os.path.join(path_to_save, filename))
-    splitter.find_points()
-    return jsonify({'points': splitter.get_frame_points()})
+    time_points, frame_points, frames = run_splitter(num_threads=3, file_path=filename)
+    return jsonify({'frame_points': frame_points})
+
+
+@app.route('/videos', methods=['POST'])
+def get_frame_points():
+    filename = save_video(request=request)
+    time_points, frame_points, frames = run_splitter(num_threads=3, file_path=filename)
+    write_videos(time_points, frame_points, frames, file_path=filename, path_to_save=r'utils/')
+    video_zip = zip_videos(r'utils/')
+    return send_file(video_zip, attachment_filename='capsule.zip', as_attachment=True)
