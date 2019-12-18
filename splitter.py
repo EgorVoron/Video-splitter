9440d4d047611f659a8ae87f3535cde5a4959e35
EgorVoron <ekvoron01@gmail.com>
2019-12-18 06:31:58 +0300

    :ambulance: Add multiprocessing

--- splitter.py
+++ splitter.py
@@ -1,37 +1,37 @@
 from moviepy.editor import *
 from skimage.measure import compare_ssim
 import cv2
-import imageio
+import threading
 
 
-class Splitter:
+class Video:
     def __init__(self, video_path):
         self.video_clip = VideoFileClip(video_path)
-        self.len = self.video_clip.duration
         self.fps = self.video_clip.fps
+        self._len = self.video_clip.duration
+        self._frame_number = self._len * self.fps
+        self._start = 0
+        self._stop = self._len
         self.frame_time = 1 / self.fps  # time per one frame
         self.frame_points = [0, ]  # frame points after each cut
-        self.split_rate = 0.45
         self.time_points = [0, ]
         self.frames = []
 
-    @staticmethod
-    def get_diff(im1, im2, mode='gray'):
-        im1 = cv2.resize(im1, (200, 200))
-        im2 = cv2.resize(im2, (200, 200))
-        if mode == 'color':
-            im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
-            im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-        (score, diff) = compare_ssim(im1, im2, full=True)
-        return score
+    def make_video_part(self, part, parts_num):
+        self._len = self._len / parts_num
+        self._frame_number = self._frame_number / parts_num
+        self._start = self._len * part
+        if part == 0:
+            self._start = self.frame_time
+        self._stop = self._len * (part + 1)
 
     def find_points(self, accuracy=9):
-        previous_frame = self.video_clip.get_frame(self.frame_time)
-        current_time_point = self.frame_time * 2
-        while current_time_point < self.len:
+        previous_frame = self.video_clip.get_frame(self._start)
+        current_time_point = self._start + self.frame_time
+        while current_time_point < self._stop:
             current_frame = self.video_clip.get_frame(current_time_point)
-            similarity = self.get_diff(previous_frame, current_frame, mode='color')
-            if similarity < self.split_rate:
+            similarity = get_diff(previous_frame, current_frame, mode='color')
+            if similarity < 0.30:
                 self.time_points.append(round(current_time_point, accuracy))
                 self.frame_points.append(round(current_time_point * self.fps))
             previous_frame = current_frame
@@ -58,11 +58,64 @@ class Splitter:
     #                     self.fps)
 
 
-# from time import time as t
-#
-# s = t()
-# splitter = Splitter(video_path='11.mp4')
-# splitter.find_points()
-# print('frames:', splitter.get_frame_points())
-# print('time points:', splitter.get_time_points())
-# print('TIME:', t() - s)
+def get_diff(im1, im2, mode='gray'):
+    im1 = cv2.resize(im1, (200, 200))
+    im2 = cv2.resize(im2, (200, 200))
+    if mode == 'color':
+        im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+        im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+    (score, diff) = compare_ssim(im1, im2, full=True)
+    return score
+
+
+class MyThread(threading.Thread):
+    def __init__(self, number):
+        threading.Thread.__init__(self)
+        self.number = number
+        self._return = None
+
+    def run(self):
+        video = Video(video_path='11.mp4')
+        video.make_video_part(self.number, 3)
+        video.find_points()
+        message = "%s is running" % self.number
+        print(message)
+        self._return = video.get_time_points()
+
+    def join(self, *args):
+        threading.Thread.join(self)
+        return self._return
+
+
+def create_threads():
+    threads = []
+    for part in range(3):
+        my_thread = MyThread(part)
+        threads.append(my_thread)
+        my_thread.start()
+    for thread in threads:
+        print(thread.join())
+
+
+def old():
+    from time import time as t
+    s = t()
+    video = Video(video_path='22.mp4')
+    video.find_points()
+    dt = t() - s
+    print(f'SPEED: {(video._len * video.fps) / dt} files per second')
+    print(f'SPEED: {video._len / dt} video seconds per second')
+    print('TIME:', dt)
+    print('frames:', video.get_frame_points())
+    print('time points:', video.get_time_points())
+
+
+def new():
+    from time import time as t
+    s = t()
+    create_threads()
+    print('TIME:', t() - s)
+
+
+old()
+# new()
