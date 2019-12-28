d45bbe51b0aac308b5c940ec81b77858bee260f8
EgorVoron <ekvoron01@gmail.com>
2019-12-28 21:55:39 +0300

    :sparkles: Frame finder added

--- frame_finder.py
+++ frame_finder.py
@@ -1,6 +1,8 @@
 from splitter import Video
 from imageio import imread
-
+import numpy as np
 
 video = Video('11.mp4')
-frame = imread('')
+frame = imread('ssim/jpg/from_vif.PNG')
+np_frame = np.array(frame)
+print(video.find_frame(np_frame))
