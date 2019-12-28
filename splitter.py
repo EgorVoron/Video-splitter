48e8b9e1b78a9fe7fde3a8e971c050ae4bbec043
EgorVoron <ekvoron01@gmail.com>
2019-12-28 21:10:11 +0300

    :ambulance: for python3.5 f{}

--- splitter.py
+++ splitter.py
@@ -45,17 +45,17 @@ class Video:
             previous_frame = current_frame
             current_time_point += self.frame_time
 
-    def make_videos(self, path_to_save, thread_number):
-        for point_number in range(len(self.time_points) - 1):
-            try:
-                sub = self.video_clip.subclip(self.time_points[point_number],
-                                              self.time_points[point_number + 1] - self.frame_time)
-                audio = self.audio_clip.subclip(self.time_points[point_number],
-                                                self.time_points[point_number + 1] - self.frame_time)
-                sub.set_audio(audio)
-                sub.write_videofile(f'{path_to_save}/{thread_number}_{point_number}.mp4')
-            except Exception as exp:
-                print(exp)
+    # def make_videos(self, path_to_save, thread_number):
+    #     for point_number in range(len(self.time_points) - 1):
+    #         try:
+    #             sub = self.video_clip.subclip(self.time_points[point_number],
+    #                                           self.time_points[point_number + 1] - self.frame_time)
+    #             audio = self.audio_clip.subclip(self.time_points[point_number],
+    #                                             self.time_points[point_number + 1] - self.frame_time)
+    #             sub.set_audio(audio)
+    #             sub.write_videofile(f'{path_to_save}/{thread_number}_{point_number}.mp4')
+    #         except Exception as exp:
+    #             print(exp)
 
     # @staticmethod
     # def get_ssim(im1, im2):
